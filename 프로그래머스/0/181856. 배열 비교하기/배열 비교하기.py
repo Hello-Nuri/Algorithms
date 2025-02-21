f9ebf56c07a2554207365eefacf8ed7ea45c3d2d
Nuri park <nuri.park020@gmail.com>
2025-02-21 13:06:50 +0900

    [level 0] Title: 배열 비교하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.py
@@ -0,0 +1,12 @@
+def solution(arr1, arr2):
+    answer = 0
+    if len(arr1) > len(arr2):
+        answer += 1
+    elif len(arr1) < len(arr2):
+        answer -= 1
+    else:
+        if sum(arr1) > sum(arr2):
+            answer += 1
+        elif sum(arr1) < sum(arr2):
+            answer -= 1
+    return answer
